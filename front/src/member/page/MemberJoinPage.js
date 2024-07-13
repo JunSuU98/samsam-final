e6280d6a85ad24aaa6774f6a690a7a647af12dd2
JunSuU98 <qoreh1216@gmail.com>
2024-07-14 01:46:58 +0900

    ui: 회원가입 아이디 중복 확인 버튼 수정, 인풋과 버튼 크기 조정

--- front/src/member/page/MemberJoinPage.js
+++ front/src/member/page/MemberJoinPage.js
@@ -15,6 +15,9 @@ import { Link } from "react-router-dom";
 
 import LoginModal from "../modal/LoginModal";
 
+import { FaCheck } from "react-icons/fa6";
+
+
 function MemberJoinPage({handleStorageChange, memberId}){
     const navigate = useNavigate();
     const [show, setShow] = useState(false);
@@ -48,6 +51,9 @@ function MemberJoinPage({handleStorageChange, memberId}){
     // id 중복확인용 변수
     const [idCheckResult, setIdCheckResult] = useState(0);
 
+    const [idCheckMessage, setIdCheckMessage] = useState("");
+    const [idCheckColor, setIdCheckColor] = useState("");
+
 
     // form 입력값 변경 핸들러
     const handleChange = (e) => {
@@ -59,6 +65,8 @@ function MemberJoinPage({handleStorageChange, memberId}){
 
         if (name === 'member_id'){ // id input 의 입력값이 변경될 때 idCheckResult 를 0 으로 초기화한다
             setIdCheckResult(0);
+            setIdCheckMessage("입력중...");
+            setIdCheckColor("gray");
         }
     }
 
@@ -105,17 +113,22 @@ function MemberJoinPage({handleStorageChange, memberId}){
             const response = await axios.get(`/id-check/${formData.member_id}`);
             if(response.data === 0){ // 아이디 중복 확인을 통과한 경우
                 setIdCheckResult(1);
-                alert("사용 가능한 아이디입니다.")
+                setIdCheckMessage("사용 가능한 아이디입니다.");
+                setIdCheckColor("green");
             } else { // 아이디 중복 확인을 실패한 경우
                 setIdCheckResult(0);
-                alert("사용할 수 없는 아이디입니다.")
+                setIdCheckMessage("사용할 수 없는 아이디입니다.");
+                setIdCheckColor("red");
             }
         } catch (error) {
             console.log('id check Error: ', error);
+            setIdCheckMessage("사용할 수 없는 아이디입니다.");
+            setIdCheckColor("red");
             setIdCheckResult(0);
         }
 
     }
+
     
     return(
         <div>
@@ -129,60 +142,75 @@ function MemberJoinPage({handleStorageChange, memberId}){
                         <Form onSubmit={handleSubmit}>
 
                             <Form.Group className="mb-3">
-                                <Form.Label style={{fontWeight: 'bold'}}>아이디</Form.Label>
-
                                 <Row>
-                                    <Col sm={9}>
-                                        <Form.Control type="text" placeholder="아이디 입력" name="member_id" id="member_id" value={formData.member_id} onChange={handleChange}/>
+                                    <Col>
+                                        <Form.Label style={{fontWeight: 'bold'}}>아이디</Form.Label>
                                     </Col>
-                                    <Col sm={3}>
-                                        <Button id="id_check" onClick={idCheck} value={0} style={{ backgroundColor: 'white', color: '#21E6C1', borderColor: '#21E6C1', width: '100%', fontWeight:'bold'}}>중복확인</Button>
+                                    <Col className="d-flex justify-content-end">
+                                        {idCheckMessage && <div style={{ color: `${idCheckColor}` , display:'inline'}}>{idCheckMessage}</div>}
                                     </Col>
                                 </Row>
 
+                                <Row>
+                                    <Col sm={9}>
+                                        <Form.Control type="text" placeholder="아이디 입력" name="member_id" id="member_id" value={formData.member_id} onChange={handleChange} style={{ height: '45px' }}/>
+                                    </Col>
+                                    <Col sm={3}>
+                                        { idCheckResult === 1 ?
+                                            <Button id="id_check"  style={{ backgroundColor: '#21E6C1', color: 'white', borderColor: '#21E6C1', width: '100%', height: '45px', fontWeight:'bold'}}>
+                                                <FaCheck style={{fontSize: '26px'}}/>
+                                            </Button>                                           
+                                            :
+                                            <Button id="id_check" onClick={idCheck} value={0} style={{ backgroundColor: 'white', color: '#21E6C1', borderColor: '#21E6C1', width: '100%', height: '45px', fontWeight:'bold'}}>
+                                                중복확인 
+                                            </Button>
+                                        }
+
+                                    </Col>
+                                </Row>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>비밀번호</Form.Label>
-                                <Form.Control type="password" placeholder="비밀번호 입력" name="member_password" id="member_password" value={formData.member_password} onChange={handleChange}/>
+                                <Form.Control type="password" placeholder="비밀번호 입력" name="member_password" id="member_password" value={formData.member_password} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>비밀번호 재확인</Form.Label>
-                                <Form.Control type="password" placeholder="비밀번호 재입력" name="confirm_password" id="confirm_password" value={formData.confirm_password} onChange={handleChange}/>
+                                <Form.Control type="password" placeholder="비밀번호 재입력" name="confirm_password" id="confirm_password" value={formData.confirm_password} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>이름</Form.Label>
-                                <Form.Control type="text" placeholder="이름 입력" name="member_name" id="member_name" value={formData.member_name} onChange={handleChange}/>
+                                <Form.Control type="text" placeholder="이름 입력" name="member_name" id="member_name" value={formData.member_name} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>이메일</Form.Label>
-                                <Form.Control type="email" placeholder="이메일 입력" name="member_email" id="member_email" value={formData.member_email} onChange={handleChange}/>
+                                <Form.Control type="email" placeholder="이메일 입력" name="member_email" id="member_email" value={formData.member_email} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>전화번호</Form.Label>
-                                <Form.Control type="text" placeholder="전화번호 입력 ('-'포함 13자리 입력)" name="member_phone" id="member_phone" value={formData.member_phone} onChange={handleChange}/>
+                                <Form.Control type="text" placeholder="전화번호 입력 ('-'포함 13자리 입력)" name="member_phone" id="member_phone" value={formData.member_phone} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>주소</Form.Label>
-                                <Form.Control type="text" placeholder="주소 입력" name="member_address" id="member_address" value={formData.member_address} onChange={handleChange}/>
+                                <Form.Control type="text" placeholder="주소 입력" name="member_address" id="member_address" value={formData.member_address} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Form.Group className="mb-3">
                                 <Form.Label style={{fontWeight: 'bold'}}>생년월일</Form.Label>
-                                <Form.Control type="date" name="member_birth" id="member_birth" value={formData.member_birth} onChange={handleChange}/>
+                                <Form.Control type="date" name="member_birth" id="member_birth" value={formData.member_birth} onChange={handleChange} style={{ height: '45px' }}/>
                             </Form.Group>
 
                             <Row>
                                 <Col sm={4}>
-                                    <Button style={{width:'100%', borderColor:'#7B7B7B', backgroundColor:'white', color:'#7B7B7B', fontWeight:'bold'}} onClick={() => {navigate(-1)}}>취소 </Button>
+                                    <Button style={{width:'100%', borderColor:'#7B7B7B', backgroundColor:'white', color:'#7B7B7B', fontWeight:'bold', fontSize:'20px', height:'50px'}} onClick={() => {navigate(-1)}}>취소 </Button>
                                 </Col>
                                 <Col sm={8}>
-                                    <Button type="submit" style={{width: '100%', border: 'none', backgroundColor: '#21E6C1', fontWeight:'bold'}}>회원가입</Button>
+                                    <Button type="submit" style={{width: '100%', border: 'none', backgroundColor: '#21E6C1', fontWeight:'bold', fontSize:'20px', height:'50px'}}>회원가입</Button>
                                 </Col>
                             </Row>
 
